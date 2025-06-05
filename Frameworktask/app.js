1b262ac46e1c5919d6d61117f6e33823942e6f9c
akshfs <akshada.aachrekar@orange.com>
2025-06-05 11:49:25 +0530

    express framework
    test

---
+++ Frameworktask/app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/users', (req, res) => {
+    console.log(req.body);
+  res.send('Hello World for users!!!!')
+})
+
+app.get('/posts', (req, res) => {
+  res.send('Hello World for posts!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ Hello.ts
@@ -0,0 +1,11 @@
+//const name_2 = "World";
+//console.log(name_2);
+// const obj = {
+//     name: 'Rahul' 
+// }
+// obj.name =  'Vikash';
+// console.log("name : " + JSON.stringify(obj));
+let name_4:boolean = true;
+console.log(name_4);
+// let name_4:string[] = ['1','2'];
+// console.log(name_4);
