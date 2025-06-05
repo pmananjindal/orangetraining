432831f07313de71ca1da5f551bdf025863189d5
Swati-singh-orange <swati.singh@orange.com>
2025-06-05 10:13:28 +0530

    move npm folder

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,9 @@
+import {randomSuperhero} from 'superheroes';
+
+
+//var generateName = require('sillyname');
+//var sillyName = generateName();
+
+//console.log("Hi, I am " + sillyName);
+
+console.log("Hi, I am "+ randomSuperhero());
