0f102c56310fa1afaabc9d7ddbf639d840241dfe
muralitalluri08 <muralikrishna.talluri@orange.com>
2025-06-05 11:49:46 +0530

    update

--- app.js
+++ app.js
@@ -1,79 +1,18 @@
-//const myModule = require('./myModule');
-//myModule.sayHello();
-//myModule.sayGoodbye();
- 
-//const fs = require('fs');
-//fs.readFile('./myModule'.'utf8',(err,data)=>{
-//if(err) throw err;
-//console.log(data);
-//}
-//)
- 
-//function greet(name, callback) {
-//  console.log('hi' + '' + name);
-//  callback();
-//}
- 
-//function callme() {
-//  console.log('Iam a call back function');
-//}
- 
-//greet('Peter', callme);
-//const fs = require('fs');
-//fs.mkdir('mydir', (err)=>{
-  //  if(err) throw err;
-  //  console.log('Directory Cretaed');
-//}
-(function(){
-    var doAsync = function(){
-        var p = new Promise(function(resolve,reject){
-            setTimeout(function(){
-                resolve({msg:'doAsync promise fulfilled'});
-                //reject({msg:'doAsync promise rejected'});
-            },5000);
-        });
-        return p;
-    }
-    
-    doAsync().then(function(response){
-        console.log("Success : ",response.msg);
-    },function(response){
-        console.log("Failed : ",response.msg);
-    });
-})();
-const EventEmitter = require('events');
-const emitter = new EventEmitter();
-
-emitter.on('greet',(name)=>{
-    console.log(`hello, ${name}`);
-});
-
-{
-    function fetchData(){
-        return new Promise((resolve)=>{
-            setTimeout(()=>{
-                resolve("Data received");
-            }, 1000);
-        });
-    }
-}
-async function main(){
-    try{
-        const result = await fetchData();
-        console.log(result);
-
-    }catch(error){
-        console.error("Error:",error);
-    }
-    
-}
-main();
-const http = require ("http");
-const server = http.createServer(function(req,res){
-    res.end("Hello world");
-});
-server.listen(3001,function(){
-    console.log("Server is running on port 3001 nowdd");
+const express = require('express')
+const app = express()
+const port = 3004
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.post('/users', (req, res) => {
+   // console.log(req.body);
+  res.send('Hello World! for users 2')
+})
+app.get('/posts', (req, res) => {
+  res.send('Hello World!for ports')
 })
 
 
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
