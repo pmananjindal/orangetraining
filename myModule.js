5b3544097e3bca9f44d7ad09ee7271fb049c9776
Reenabpatil <32438236+Reenabpatil@users.noreply.github.com>
2025-06-05 09:37:21 +0530

    Create myModule.js

---
+++ myModule.js
@@ -0,0 +1,9 @@
+const greetings = {
+    sayHello:() => {
+        console.log("Hello, World!");
+    },
+    sayGoodbye:() => {
+        console.log("Goodbye, World!");
+    }
+};
+module.exports = greetings;
