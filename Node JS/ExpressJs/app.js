c99aad38f2ff34dc60b0ca926dbc9aa10c80d6f7
Jignesh Shah <jignesh.a.shah@orange.com>
2025-06-06 09:04:53 +0530

    users CRUD endpoints

--- Node JS/ExpressJs/app.js
+++ Node JS/ExpressJs/app.js
@@ -12,10 +12,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/users', (req, res) => {
-    res.send('Hello Users!')
-  })
-
 app.get('/test', (req, res) => {
   res.send('Hello Tests!!!')
 })
@@ -24,23 +20,41 @@ app.get('/posts', (req, res) => {
     res.send('Hello Posts!')
 })
 
+app.get('/products/:prodid/:name/:price/:color', (req, res) => {
+  const productId = req.params.prodid;
+  const  prodname = req.params.name;
+  const price = req.params.price;
+  const color = req.params.color; 
+  const product = {id:productId, name:prodname,price:price,color:color}
+  res.json(product);
+})
+
+var userData = [];
 app.post('/users', (req, res) => {
-    res.send('user created!')
-    console.log(req.body);
-
-    // const decoder = new stringDecoder('utf8');
-    // let buffer='';
-
-    // req.on('data',function(data){
-    //     buffer += decoder.write(data)
-    // })
-    // req.on('end',function(){
-    //     buffer += decoder.end()
-    //     // res.end("hello Bye")
-    //     console.log(buffer);
-    // })
-
+    const userParam = req.body;
+    userParam.userId = 1;
+    userData.push(req.body);
+    
+    res.send('user created successfully!');
   })
+
+app.get('/users', (req, res) => {
+  res.json(userData);
+})
+
+app.put('/users/:userId/:address', (req, res) => {
+  var userId = req.params.userId;
+  var address = req.params.address;
+
+  userData[0]["address"] = address;
+  res.send(`User with Id ${userId} has been updated with Address:${address} !`)
+})
+
+app.delete('/users/:userId', (req, res) => {
+  var userId = req.params.userId;
+  userData.pop();
+  res.send(`User with Id ${userId} has been deleted !`)
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
