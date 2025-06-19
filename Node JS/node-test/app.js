1e0ca8ebfaa638ef641f6ff3f6ecb3b30451f53a
Jignesh Shah <jignesh.a.shah@orange.com>
2025-06-19 10:52:02 +0530

    setup for supertest test framework

--- Node JS/node-test/app.js
+++ Node JS/node-test/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 app.get("/", (req, res) => {
-  res.send("Hello, World!!");
+  //  res.send("Hello, World!!");
+   res.json({"message":"Hello, World!!"})
 });
 module.exports = app;
---
+++ Node JS/node-test/test/app.test.js
@@ -0,0 +1,13 @@
+const request = require('supertest');
+const {expect} = require('chai');
+const app = require('../app');
+
+describe('GET /hello',()=>{
+
+    it('should return hello world message', async () => {
+        const res = await request(app).get('/');
+
+        expect(res.status).to.equal(200);
+        expect(res.body).to.have.property("message","Hello, World!!");
+    });
+})
