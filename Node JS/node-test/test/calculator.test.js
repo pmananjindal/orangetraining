c55562000fc9db4ba0d9427030932f1199519d42
Jignesh Shah <jignesh.a.shah@orange.com>
2025-06-19 09:59:18 +0530

    setup for chai mocha

---
+++ Node JS/node-test/test/calculator.test.js
@@ -0,0 +1,33 @@
+const {add, subtract} = require('../utility/calculator');
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Calculator',() =>{
+
+    describe('add',() =>{
+   
+        it('should return 5 for add(2,3)',() =>{
+            expect(add(2,3)).to.equal(5);
+        })
+
+        it('should return -1 for add(-2,1)',() =>{
+            expect(add(-2,1)).to.equal(-1);
+        })
+    });
+
+    describe('subtract',() =>{
+   
+        it('should return 1 for subtract(3,2)',() =>{
+            expect(subtract(3,2)).to.equal(1);
+        });
+        
+        it('should return -3 for subtract(1, 4)', () => {
+            expect(subtract(1, 4)).to.equal(-3);
+        });
+
+        it('should return 3 for subtract(7,4)',() =>{
+            expect(subtract(7,4)).to.equal(3);
+        });
+    });
+
+});
