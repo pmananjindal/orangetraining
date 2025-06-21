b254323557a62b655f83d0009bd32fc5dc7a9105
akshfs <akshada.aachrekar@orange.com>
2025-06-21 19:58:42 +0530

    Delete user-test directory

--- user-test/app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const userRoutes = require("./routes/userRoutes");
-app.use(express.json());
-app.get("/", (req, res) => {
-    res.json({ message: 'Hello, world!' });
-});
-app.use("/users", userRoutes);
-module.exports = app;
--- user-test/server.js
+++
@@ -1,7 +0,0 @@
-const connectDB = require("./db");
-connectDB();
-const app = require("../app");
-const port = 3000;
-app.listen(port, () => {
-    console.log(`server is running on port ${port}`);
-});
